fd4cca4dee7dff69235c492d8df4e4879f386941
stcalica <kycalica@gmail.com>
2024-05-03 17:57:28 -0700

    fix pricing tiers

--- constants/tiers.tsx
+++ constants/tiers.tsx
@@ -2,21 +2,21 @@ export const tiers = [
     {
       id: "0",
       title: "Early Registration",
-      price: parseFloat("$65"),
+      price: parseFloat("65"),
       startDate: "",
       endDate: ""
     },
     {
         id: "1",
         title: "Regular Registration",
-        price:  parseFloat("$85"),
+        price:  parseFloat("85"),
         startDate: "",
         endDate: ""
     },
     {
         id: "2",
         title: "Late Registration",
-        price:  parseFloat("$105"),
+        price:  parseFloat("105"),
         startDate: "",
         endDate: ""
     },
--- templates/HomePage/Pricing/index.tsx
+++ templates/HomePage/Pricing/index.tsx
@@ -3,7 +3,6 @@ import PricingTier from "@/components/PricingTiers";
 import { tiers } from "@/constants/tiers";
 
 type PropTypes = {};
-console.log("tiers", tiers);
 
 const Pricing = ({}: PropTypes) => (
     <div className="w-full flex flex-col items-center justify-center">  
