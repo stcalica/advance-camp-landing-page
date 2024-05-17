ba2b92d633ff3154ba0e61c221d2dc7b1f5edd47
stcalica <kycalica@gmail.com>
2024-05-17 04:47:12 -1000

    changed to aviation

--- templates/FormsPage/Main/index.tsx
+++ templates/FormsPage/Main/index.tsx
@@ -7,7 +7,7 @@ type MainProps = {};
 const Main = ({}: MainProps) => (
     <div className="py-20">
         <div className="text-center text-h1 mb-16">Important Forms</div>
-        <a href="/forms/19-673.pdf" target="_" className="text-center text-h4 mb-16"><h3>Activity Permissions Form</h3></a>
+        <a href="/forms/19-673.pdf" target="_" className="text-center text-h4 mb-16"><h3>Aviation Permissions Form</h3></a>
     </div>
 );
 
