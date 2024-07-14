f1169eeab5649b3d073556798c548154281e9d98
psmith586 <psmith586@yahoo.com>
2024-07-14 15:13:15 -0700

    require troop no. on checkout

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -5,30 +5,40 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: NextRequest) {
 
-    const resp = await req.json();
+  const resp = await req.json();
 
-    try {
+  try {
 
-        const data = JSON.parse(resp.body)
-        const priceId = data.priceId
+    const data = JSON.parse(resp.body)
+    const priceId = data.priceId
 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            line_items: [
-                {
-                    price: priceId,
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: `${process.env.HOST}/products/success`,
-            cancel_url: `${process.env.HOST}/products/canceled`,
-        });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.HOST}/products/success`,
+      cancel_url: `${process.env.HOST}/products/canceled`,
+      custom_fields: [
+        {
+          key: "troop_number",
+          label: {
+            type: "custom",
+            custom: "Troop Number"
+          },
+          type: "text"
+        }
+      ]
+    });
 
-        console.log("redirect: ", session.url)
+    console.log("redirect: ", session.url)
 
-        return NextResponse.json({ session: session, ok: true });
-    } catch (err: any) {
-        return NextResponse.json({ error: err.message, ok: false }, { status: 500 });
-    }
+    return NextResponse.json({ session: session, ok: true });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message, ok: false }, { status: 500 });
+  }
 }
