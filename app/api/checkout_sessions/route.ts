e7b11eeb40b1f74975b29669488241edc43ad99e
psmith586 <psmith586@yahoo.com>
2024-07-31 17:19:27 -0700

    fix local build crash

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export async function POST(req: NextRequest) {
 
